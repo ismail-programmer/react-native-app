43d2815eee03716eae0f2a861e9ec92801d4446f
Ismail <lord28062000@gmail.com>
2019-09-25 16:30:27 +0500

    navigation part 1 done

--- components/Dishdetail.js
+++ components/Dishdetail.js
@@ -1,35 +1,42 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Text, View } from 'react-native';
 import { Card } from 'react-native-elements';
+import { DISHES } from '../shared/dishes';
 
 function RenderDish(props) {
+  const dish = props.dish;
 
-    const dish = props.dish;
-    
-        if (dish != null) {
-            return(
-                <Card
-                featuredTitle={dish.name}
-                image={require('./images/uthappizza.png')}>
-                    <Text style={{margin: 10}}>
-                        {dish.description}
-                    </Text>
-                </Card>
-            );
-        }
-        else {
-            return(<View>
-
-<Text>
-Please Select an item!
-</Text>
-
-            </View>);
-        }
+  if (dish != null) {
+    return (
+      <Card
+        featuredTitle={dish.name}
+        image={require('./images/uthappizza.png')}
+      >
+        <Text style={{ margin: 10 }}>{dish.description}</Text>
+      </Card>
+    );
+  } else {
+    return <View></View>;
+  }
 }
 
-function Dishdetail(props) {
-    return(<RenderDish dish={props.dish} />);
+class DishDetail extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      dishes: DISHES
+    };
+  }
+
+  static navigationOptions = {
+    title: 'Dish Details'
+  };
+
+  render() {
+    const dishId = this.props.navigation.getParam('dishId', '');
+    return <RenderDish dish={this.state.dishes[+dishId]} />;
+  }
 }
 
-export default Dishdetail;
+export default DishDetail;
